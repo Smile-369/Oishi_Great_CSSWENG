b833d0a9f7dd1f1a9a7ab3192531dce392f01ab8
hcris2 <135817006+hcris2@users.noreply.github.com>
2024-04-06 01:07:20 +0800

    opafsogpojuaoig
    gajkfsahjk

--- script/location.js
+++ script/location.js
@@ -3,9 +3,9 @@ const scriptURL = 'https://script.google.com/macros/s/AKfycbxL4ZgKC5zYijdg7JtwFK
 const form = document.forms['inquiry-form']
 
 form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => alert("Thank you! Your inquiry was submitted successfully." ))
-    .then(() => { window.location.reload(); })
-    .catch(error => console.error('Error!', error.message))
+   e.preventDefault()
+   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+   .then(response => alert("Thank you! Your inquiry was submitted successfully." ))
+   .then(() => { window.location.reload(); })
+   .catch(error => console.error('Error!', error.message))
 })
